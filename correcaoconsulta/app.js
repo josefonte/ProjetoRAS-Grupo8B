861a9cd5c38d57b8f6e037eb45e9459574fc5260
rafaelcorreia94870 <a94870@alunos.uminho.pt>
2023-12-10 16:29:55 +0000

    sql a dar

--- correcaoconsulta/app.js
+++ correcaoconsulta/app.js
@@ -13,6 +13,8 @@ var seeRouter = require('./routes/see');
 var app = express();
 
 
+
+
 const connection = mysql.createConnection({
   host: `localhost`,
   user: `gajofixe`,
@@ -20,17 +22,83 @@ const connection = mysql.createConnection({
   database: `fixe`,
 });
 
-const script = fs.readFileSync('mysql/fixe.sql', 'utf8');
-
-connection.query(script, (err, results) => {
+const resetTables = `
+SELECT CONCAT('DROP TABLE IF EXISTS ', table_name, ';')
+FROM information_schema.tables
+WHERE table_schema = 'fixe';
+`
+connection.query(resetTables, (err, results) => {
   if (err) {
-    console.error('Error executing SQL script:', err.message);
+    console.error('Error deleting tables:', err);
   } else {
-    console.log('SQL script executed successfully');
-  }
+    console.log('Tables deleted successfully');
+  }}
+)
 
-  connection.end();
-});
+const createTableProva=`
+CREATE TABLE IF NOT EXISTS Prova (
+  id_prova_realizada VARCHAR(255) NOT NULL,
+  id_prova_duplicada VARCHAR(255) NOT NULL,
+  classificacao_final INT(10) NULL,
+  num_Aluno VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id_prova_realizada));
+`
+connection.query(createTableProva, (err, results) => {
+  if (err) {
+    console.error('Error creating table Prova:', err);
+  } else {
+    console.log('Prova table created successfully');
+  }}
+)
+
+
+const createTableTipoQuestao = `
+CREATE TABLE IF NOT EXISTS TipoQuestao(
+  id_tipo VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id_tipo));`
+connection.query(createTableTipoQuestao, (err, results) => {
+  if (err) {
+    console.error('Error creating table TipoQuestao:', err);
+  } else {
+    console.log('TipoQuestao Table created successfully');
+  }}
+)
+
+const createTableQuestao = `
+CREATE TABLE IF NOT EXISTS Questao (
+  id_questao VARCHAR(255) NOT NULL,
+  cotacaoTotal FLOAT(10) NULL,
+  Prova_id_prova_realizada VARCHAR(255) NOT NULL,
+  TipoQuestão_id_tipo VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id_questao),
+    FOREIGN KEY (Prova_id_prova_realizada)
+    REFERENCES Prova (id_prova_realizada)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    FOREIGN KEY (TipoQuestão_id_tipo)
+    REFERENCES TipoQuestao (id_tipo)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);`
+connection.query(createTableQuestao, (err, results) => {
+  if (err) {
+    console.error('Error creating table Questao:', err);
+  } else {
+    console.log('Questao table created successfully');
+  }}
+)
+/*
+const createUser=`
+CREATE USER gajofixe@localhost IDENTIFIED BY raposeira ;
+GRANT ALL PRIVILEGES ON fixe.* TO gajofixe@localhost;
+GRANT ALL PRIVILEGES ON *.* TO gajofixe@localhost;`
+connection.query(createUser, (err, results) => {
+  if (err) {
+    console.error('Error creating table Questao:', err);
+  } else {
+    console.log('Questao table created successfully');
+  }}
+)
+*/
 
 app.use(cors()); 
 app.use(logger('dev'));
