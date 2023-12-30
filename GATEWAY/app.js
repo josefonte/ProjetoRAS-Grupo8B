8465d33625e69375cfce750f44d12d4703e82409
MiguelRaposo <73466553+MiguelRaposo@users.noreply.github.com>
2023-12-30 14:51:32 +0000

    esqueci me deste detalhe

--- GATEWAY/app.js
+++ GATEWAY/app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var correcaoconsultaRouter = require('./routes/correcaoconsulta');
 
 var app = express();
 app.use(cors());
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', correcaoconsulta);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
